164436224ed53af07b10d45d4f553e6a3bccfe0b
denispalitsyn <den.palitsyn@gmail.com>
2023-11-29 11:33:23 +0400

    api added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+import { useEffect } from 'react';
+import MovieApi from './api';
+
+const genreById = {
+  action: 28,
+  adventure: 12,
+  comedy: 35,
+  documentary: 99,
+  fantasy: 14,
+  horror: 27,
+  western: 37,
+};
+
 function App() {
+  useEffect(() => {
+    MovieApi.search('fast x').then((response) => console.log(response));
+  }, []);
+
   return <div className="App">Hi</div>;
 }
 
---
+++ src/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZmY1MThhYjE5ZTA1OTM3ZGQyYmNmNDdiZjAyMGRkZiIsInN1YiI6IjY1NWUxMTlmZDM5OWU2MDBlYzQzZGY2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0mM90q115YiuoA2kJZtxg7REXj56kUCbRkmWLGl_j90',
+  },
+});
+
+class MovieApi {
+  static async getPopular() {
+    const response = await client.get('movie/popular?language=en-US&page=1');
+
+    return response.data.results;
+  }
+
+  static async getTopRated() {
+    const response = await client.get('movie/top_rated?language=en-US&page=1');
+
+    return response.data.results;
+  }
+
+  static async getUpcoming() {
+    const response = await client.get('movie/upcoming?language=en-US&page=1');
+
+    return response.data.results;
+  }
+
+  static async getMovie(id) {
+    const response = await client.get(`movie/${id}?language=en-US`);
+
+    return response.data;
+  }
+
+  static async getMovieByGenreId(genreId) {
+    const response = await client.get(
+      `discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreId}`
+    );
+
+    return response.data.results;
+  }
+
+  static async search(query) {
+    const response = await client.get(
+      `search/movie?query=${query}&include_adult=false&language=en-US&page=1`
+    );
+
+    return response.data.results;
+  }
+}
+
+export default MovieApi;
