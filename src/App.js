cf9855d6fe5dda73081141359089d471bd61beb4
denispalitsyn <den.palitsyn@gmail.com>
2023-11-30 20:13:21 +0400

    tailwind, movies on page added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import MovieApi from './api';
 
 const genreById = {
@@ -12,11 +12,123 @@ const genreById = {
 };
 
 function App() {
+  const [movies, setMovies] = useState({
+    upcoming: [],
+    popular: [],
+    topRated: [],
+    adventure: [],
+    comedy: [],
+    action: [],
+    horror: [],
+    fantasy: [],
+    documentary: [],
+    western: [],
+  });
+
   useEffect(() => {
-    MovieApi.search('fast x').then((response) => console.log(response));
+    Promise.all([
+      MovieApi.getUpcoming(),
+      MovieApi.getPopular(),
+      MovieApi.getTopRated(),
+      MovieApi.getMovieByGenreId(genreById.adventure),
+      MovieApi.getMovieByGenreId(genreById.comedy),
+      MovieApi.getMovieByGenreId(genreById.action),
+      MovieApi.getMovieByGenreId(genreById.horror),
+      MovieApi.getMovieByGenreId(genreById.fantasy),
+      MovieApi.getMovieByGenreId(genreById.documentary),
+      MovieApi.getMovieByGenreId(genreById.western),
+    ])
+      .then((responses) =>
+        setMovies({
+          upcoming: responses[0],
+          popular: responses[1],
+          topRated: responses[2],
+          adventure: responses[3],
+          comedy: responses[4],
+          action: responses[5],
+          horror: responses[6],
+          fantasy: responses[7],
+          documentary: responses[8],
+          western: responses[9],
+        })
+      )
+      .catch((error) => console.log(error));
   }, []);
 
-  return <div className="App">Hi</div>;
+  console.log('movies', movies);
+
+  // https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg
+
+  const movieList = [
+    {
+      key: 'upcoming',
+      title: 'Upcoming Movies',
+    },
+    {
+      key: 'popular',
+      title: 'Popular Movies',
+    },
+    {
+      key: 'topRated',
+      title: 'Top Rated Movies',
+    },
+    {
+      key: 'adventure',
+      title: 'Adventure Movies',
+    },
+    {
+      key: 'comedy',
+      title: 'Comedy Movies',
+    },
+    {
+      key: 'action',
+      title: 'Action Movies',
+    },
+    {
+      key: 'horror',
+      title: 'Horror Movies',
+    },
+    {
+      key: 'fantasy',
+      title: 'Fantasy Movies',
+    },
+    {
+      key: 'documentary',
+      title: 'Documentary Movies',
+    },
+    {
+      key: 'western',
+      title: 'Western Movies',
+    },
+  ];
+
+  return (
+    <div className="container mx-auto text-3xl">
+      {movieList.map(({ key, title }) => (
+        <Fragment key={title + key}>
+          <h2 className="mb-5 font-bold mt-10">{title}</h2>
+          <div className="overflow-x-auto">
+            <div className="flex gap-7">
+              {movies[key].map(({ poster_path, title, id }) => (
+                <div
+                  key={key + id}
+                  className="bg-slate-400 min-w-[150px] h-[225px] overflow-hidden cursor-pointer"
+                >
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200${poster_path}`}
+                    alt={title}
+                    width={150}
+                    height={225}
+                    className="hover:scale-125 transition-all duration-700"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </Fragment>
+      ))}
+    </div>
+  );
 }
 
 export default App;
